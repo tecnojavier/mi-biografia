7aad0a7ebf6fc05070db7bbd4ee5c1179721d26c
Javier Bonilla <150548751+tecnojavier@users.noreply.github.com>
2025-08-08 00:32:51 -0600

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,16 @@
+// Animación al hacer scroll
+const sections = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+sections.forEach(section => {
+  observer.observe(section);
+});
