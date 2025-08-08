7e630da0ee3771611ea99c3f2d82d0243b944b06
Javier Bonilla <150548751+tecnojavier@users.noreply.github.com>
2025-08-08 00:46:19 -0600

    functions.js

--- functions.js
+++ functions.js
@@ -14,3 +14,10 @@ const observer = new IntersectionObserver(entries => {
 sections.forEach(section => {
   observer.observe(section);
 });
+
+const toggleBtn = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+toggleBtn.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
